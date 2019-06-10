5bae79a570283da7f6013bf548e2864145b23326
tiihsilva <tiagoxd100@gmail.com>
2019-06-10 12:43:31 -0300

    Add cadastro de produtos

--- cadastroProduto.php
+++ cadastroProduto.php
@@ -1,35 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include_once "head.php"; ?>   
+<?php include_once "head.php"; ?>
+
 <body>
-<?php include_once "header.php"; ?>
+    <?php include_once "header.php"; ?>
 
 
-<main class="container">
-    <section class="row">
-        <div class="col-md-6">
-    <form action="salvarProduto.php" method = "post" enctype = "multipart/form-data">
+    <main class="container">
+        <section class="row">
+            <div class="col-md-6">
+                <form action="salvarProduto.php" method="post" enctype="multipart/form-data">
 
-        <div class="form-group">
-        <label for="nomeProduto">Nome do Produto</label>
-        <input type="text" class="form-control" id="nomeProduto" placeholder="Nome do Produto" name="nomeProduto">
-        </div>
-         <div class="form-group">
-         <label for="precoProduto">Preço do Produto</label>
-         <input type="number" step="any" class="form-control" id="precoProduto" placeholder="Preço do Produto" name="precoProduto">
-         </div>
-         <div class="form-group">
-          <label for="descProduto">Descrição do Produto</label>
-          <textarea class="form-control" name="descProduto" id="descProduto"></textarea>
-        </div>
-        <div class="form-group">
-         <label for="imgProduto">Descrição do Produto</label>
-         <input type="file" name="arquivo" id="imgProduto"></input>
-          </div>
-         <button class="btn btn-success" type="submit">Finalizar Cadastro</button>
-    </form>
-        </div>
-    </section>
-</main>
+                    <!-- Nome produto -->
+                    <div class="form-group">
+                        <label for="nomeProduto">Nome do Produto</label>
+                        <input type="text" class="form-control" id="nomeProduto" placeholder="Nome do Produto" name="nomeProduto">
+                    </div>
+                    <!-- Preço do produto -->
+                    <div class="form-group">
+                        <label for="precoProduto">Preço do Produto</label>
+                        <input type="number" step="any" class="form-control" id="precoProduto" placeholder="Preço do Produto" name="precoProduto">
+                    </div>
+                    <!-- Descrição do produto -->
+                    <div class="form-group">
+                        <label for="descProduto">Descrição do produto</label>
+                        <textarea class="form-control" name="descProduto" id="descProduto"></textarea>
+                    </div>
+
+                    <!-- Imagem do Produto -->
+                    <div class="form-group">
+                        <label for="imgProduto">Imagem do Produto</label>
+                        <input type="file" name="arquivo" id="imgProduto">
+                    </div>
+
+                    <button class="btn btn-success" type="submit">Finalizar cadastro</button>
+                </form>
+            </div>
+        </section>
+    </main>
+
+
+
 </body>
+
 </html>
--- head.php
+++ head.php
@@ -1,14 +1,7 @@
-<?php 
-
-include_once "funcoes.php";
-//Aula sobre funções //
-
-
-
+<?php
+include "funcoes.php";
 $usuario = logarUsuario("Tiago Ferreira", 1);
-//$usuario = "false";
-
-//Aula sobre o laço for envolvendo arrays.
+//$usuario = "";
 
 $jsonProdutos = file_get_contents('Produtos.json');
 
@@ -17,8 +10,7 @@ $produtos = json_decode($jsonProdutos, true);
 $produtos = $produtos['Produtos'];
 
 
-$categorias = ["cursos", "Tutoriais", "Artigos", "Forum", "Codigos"];
-
+$categorias = ["Cursos", "Tutoriais", "Artigos", "Forum", "Códigos"];
 ?>
 
 <head>
@@ -26,8 +18,7 @@ $categorias = ["cursos", "Tutoriais", "Artigos", "Forum", "Codigos"];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Loja Virtual</title>
-
-    <!--bootstrap-->
+    <!-- -->
+    <!-- Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-
